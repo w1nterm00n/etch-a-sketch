6d99262e7eb9401226486d39258dd5162cde5cb4
w1nterm00n <kordesovad@gmail.com>
2023-06-30 20:59:21 +0300

    add event listener on button, now it creates a grid that the user enters

--- script.js
+++ script.js
@@ -9,9 +9,7 @@ parentElement.style.gridTemplateColumns = `repeat(${n}, 1fr)`;
 var gridArray = [];
 
 for ( let i = 0; i < n; i++ ){
-
     gridArray[i] = []; 
-
         for ( let j = 0; j < n; j++ ){
             var gridCell = document.createElement('div');  
             parentElement.appendChild(gridCell);
@@ -21,7 +19,61 @@ for ( let i = 0; i < n; i++ ){
             gridArray[i][j].addEventListener('click', () => {
                 gridArray[i][j].classList.add('blackCell');
               });
-
         }
 }
 
+
+var m;
+changeGridButton.addEventListener('click', () => {
+  // очищение
+  if (n > 0) {
+    var N = n;
+    for ( let i = 0; i < N; i++ ){
+        for ( let j = 0; j < N; j++ ){
+            var gridCell = gridArray[i][j];
+            parentElement.removeChild(gridCell);
+        };
+        n--;
+    }
+    console.log('n is ', n);
+  }
+
+  if (m > 0) {
+    console.log("hello");
+    var M = m;
+    for ( let i = 0; i < M; i++ ){
+        for ( let j = 0; j < M; j++ ){
+            var gridCell = gridArray[i][j];
+            parentElement.removeChild(gridCell);
+        };
+        m--;
+    }
+    console.log('m is ', m);
+  }
+  //произошло очищение грида
+
+  //создание нового грида
+  m = +prompt ('enter the number of squares (100 is maximum)');
+
+    if (m<101) {
+      parentElement.style.gridTemplateRows = `repeat(${m}, 1fr)`;  
+      parentElement.style.gridTemplateColumns = `repeat(${m}, 1fr)`;
+  
+      for ( let i = 0; i < m; i++ ){
+          gridArray[i] = [];
+              for ( let j = 0; j < m; j++ ){
+                  var gridCell = document.createElement('div');  
+                  parentElement.appendChild(gridCell);
+                  gridCell.classList.add('whiteCell');
+                  gridArray[i][j] = gridCell;
+  
+                  gridArray[i][j].addEventListener('click', () => {
+                      gridArray[i][j].classList.add('blackCell');
+                    });
+              }
+      }
+    } else {
+      alert ("100 is maximum");
+    }
+
+});
